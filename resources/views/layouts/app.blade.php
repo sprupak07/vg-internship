a2a7bd7103d11cb119a24ec22432ceb6b4d15a29
Rupak Sapkota <sapkotarupak343@gmail.com>
2026-02-05 14:09:44 +0545

    fix: layout issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Rupak Sapkota" content="Rupak Sapkota Portfolio">
-    <title>
-        {{ config('app.name') }}
-    </title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Rupak Sapkota">
+    <meta name="description" content="Rupak Sapkota Portfolio">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', config('app.name'))</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,20 +19,33 @@
         href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
         rel="stylesheet">
 
-        <style>
-            body {
-                font-family: 'Nunito Sans', sans-serif;
-            }
-        </style>
+    <style>
+        body {
+            font-family: 'Nunito Sans', sans-serif;
+        }
+    </style>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @stack('styles')
 </head>
 
-<body>
-
-    @yield('content')
+<body class="min-h-screen bg-gray-50 text-gray-900">
 
 
+
+
+    <main class="container mx-auto px-4 py-6">
+        @if(session('error'))
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <strong class="font-bold">Error!</strong>
+                <span class="block sm:inline">{{ session('error') }}</span>
+            </div>
+        @endif
+        @yield('content')
+    </main>
+
+    @stack('scripts')
 </body>
 
 </html>
